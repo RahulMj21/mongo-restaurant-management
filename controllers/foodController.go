8461e4074bb0e22596c8321179a96974435d8a19
Rahul Mondal <rahulmondar@gmail.com>
2023-01-22 17:33:24 +0530

    feat:added updateFood controller

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"math"
 	"strconv"
 	"time"
 
@@ -12,6 +13,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
@@ -117,13 +119,72 @@ func CreateFood(c *gin.Context) {
 }
 
 func round(num float64) int {
-	return 0
+	return int(math.Round(num))
 }
 
 func toFixed(num float64, precision int) float64 {
-	return 1.0
+	output := math.Pow(10, float64(precision))
+	return float64(round(output)) / output
 }
 
 func UpdateFood(c *gin.Context) {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	menu := models.Menu{}
+	food := models.Food{}
+
+	foodId := c.Param("id")
+
+	if err := c.BindJSON(&food); err != nil {
+		c.JSON(400, gin.H{
+			"status":  "fail",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	var foodObj primitive.D
+
+	if food.Name != nil {
+		foodObj = append(foodObj, bson.E{Key: "name", Value: food.Name})
+	}
+	if food.Price != nil {
+		foodObj = append(foodObj, bson.E{Key: "price", Value: food.Price})
+	}
+	if food.FoodImage != nil {
+		foodObj = append(foodObj, bson.E{Key: "food_image", Value: food.FoodImage})
+	}
+	if food.MenuId != nil {
+		err := menuCollection.FindOne(ctx, bson.D{{Key: "menu_id", Value: food.MenuId}}).Decode(&menu)
+		if err != nil {
+			c.JSON(400, gin.H{
+				"status":  "fail",
+				"message": err.Error(),
+			})
+			return
+		}
+		foodObj = append(foodObj, bson.E{Key: "menu_id", Value: menu.MenuId})
+	}
+
+	food.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	foodObj = append(foodObj, bson.E{Key: "updated_at", Value: food.UpdatedAt})
+
+	upsert := true
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+	filter := bson.D{{Key: "food_id", Value: foodId}}
+
+	result, err := foodCollection.UpdateOne(ctx, filter, bson.D{{Key: "$set", Value: foodObj}}, &opt)
+	if err != nil {
+		c.JSON(500, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"status": "success",
+		"data":   result,
+	})
 
 }
